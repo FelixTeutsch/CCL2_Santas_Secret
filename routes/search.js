20d029f3d375532fc5cde8942bf9a7720486a8f8
Felix Teutsch <felix@teutsch.it>
2023-06-15 11:51:08 +0200

    Search Implementation; Not done Yet

--- routes/search.js
+++ routes/search.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 // TODO: implement search view
-router.all('/', (req, res, next) => res.render('searchPage'));
+router.all('/', (req, res, next) => res.render('search'));
 
 // TODO: implement search result
 router.all('/:result', (req, res, next) => res.render('searchResult'));
